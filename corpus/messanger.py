d9f9e18dd12adf7fd67b2eb4d6206b077832ab82
nawnoes <nawnoes@gmail.com>
2020-09-11 17:06:09 +0900

    신문 데이터 전처리

--- corpus/messanger.py
+++ corpus/messanger.py
@@ -5,8 +5,8 @@ import json
 from tqdm import tqdm
 
 # 파일 경로
-dir_path = "/Volumes/My Passport for Mac/00_nlp/dialog/messenger"
-save_file_name ='/Volumes/My Passport for Mac/00_nlp/dialog/messenger_data.txt'
+dir_path = "/Volumes/My Passport for Mac/00_nlp/00 모두의 말뭉치/MESSANGER"
+save_file_name ='/Volumes/My Passport for Mac/00_nlp/00 모두의 말뭉치/MESSANGER.txt'
 # 파일 경로 내 목록
 dir_list = os.listdir(dir_path)
 save_file_path = f'{save_file_name}'
@@ -23,12 +23,13 @@ for json_file_name in tqdm(dir_list):
         print('error: ', Exception)
         continue
     meta_info = json_data['document'][0]['metadata']
-    dialogs = json_data['document'][0]['utterance']
-    # print(meta_info)
-    for dialog in dialogs:
-        save_file.write(dialog['form']+'\n')
-    save_file.write('\n')
-    # print(dialogs)
+    docs = json_data['document']#[0]['utterance']
+
+    for doc in docs:
+        paragraphs = doc['utterance']
+        for paragraph in paragraphs:
+            save_file.write(paragraph['form'] + '\n')
+        save_file.write('\n')
 
 save_file.close()
 
